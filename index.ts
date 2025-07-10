ba3f4604d479f5b37b556f136789728c7df61f6c
Abdi-01 <abdialghi@gmail.com>
2025-07-10 10:28:45 +0700

    intro express API

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import http, { IncomingMessage, ServerResponse } from "http";
+import express, { Application, Request, Response } from "express";
 
 const PORT: number = 5000;
 
@@ -16,36 +16,51 @@ const dbStudent: any[] = [
 ];
 
 // Define API config
-const server = http.createServer(
-  (request: IncomingMessage, response: ServerResponse) => {
-    //
-    console.log("READ REQUEST", request.method, request.url);
-    // conditonal statement
-    if (request.method === "GET") {
-      if (request.url === "/") {
-        response.write("<h1>Intro API</h1>");
-        response.end();
-      } else if (request.url?.includes("/student")) {
-        const query = request.url.split("?")[1]; // ["/student", "email=hajra@mail.com"]
-        if (query) {
-          const filterEmail = dbStudent.filter((val: any) => {
-            return val.email === query.split("=")[1]; // ["email", "hajra@mail.com"]
-          });
-          response.write(JSON.stringify(filterEmail));
-        } else {
-          response.write(JSON.stringify(dbStudent));
-        }
-        response.end();
-      } else if (request.url === "/profile") {
-        //
-      } else {
-        //
-      }
-    }
-  }
-);
+const app: Application = express();
 
-// Run server
-server.listen(PORT, () => {
-  console.log(`API is RUNNING at http://localhost:${PORT}`);
+// General middleware config
+app.use(express.json()); // untuk membaca data dari req.body
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("<h1>Express API</h1>");
+});
+
+// GET : untuk membaca data
+app.get("/student", (req: Request, res: Response) => {
+  console.log(req.query);
+  res.send(dbStudent);
+});
+
+// POST : untuk menambah data
+app.post("/student", (req: Request, res: Response) => {
+  console.log("POST", req.body);
+  dbStudent.push(req.body);
+  res.send({
+    message: "Add data success",
+    result: dbStudent,
+  });
+});
+
+// PUT / PATCH : untuk memperbarui data
+app.patch("/student/:id", (req: Request, res: Response) => {
+  console.log("PATCH", req.params, req.body);
+  const findIdx = dbStudent.findIndex((val: any) => {
+    return val.id === parseInt(req.params.id);
+  });
+  dbStudent[findIdx] = { ...dbStudent[findIdx], ...req.body };
+  // {id:3, name:"andre", email:"andre@mail.com", name:"andre hidayat", email:"andre@mail.com"}
+  res.send({
+    message: "Update success",
+    result: dbStudent[findIdx],
+  });
+});
+
+// DELETE : untuk menghapus data
+app.delete("/student/:id", (req: Request, res: Response) => {
+  console.log("DELETE", req.params);
+  res.send("Delete data");
+});
+
+app.listen(PORT, () => {
+  console.log(`API is RUNNING http://localhost:${PORT}`);
 });
